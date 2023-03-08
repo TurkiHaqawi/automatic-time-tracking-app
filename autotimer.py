0d5470cd03209f73c8b85aa30f604ff9747f48f3
TurkiHaqawi <turki.haqawii@gmail.com>
2023-03-08 23:36:40 +0300

    Second activity to get active window

--- autotimer.py
+++ autotimer.py
@@ -1,13 +1,18 @@
-from AppKit import NSWorkspace
+from AppKit import NSWorkspace, NSAppleScript
 import time
 
 active_window_name = ""
 while True:
     
-    # First activate to get which app is being used now (macOS)
+    # First activity to get which app is being used now (macOS)
     new_window_name = (NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName'])
     if active_window_name != new_window_name:
         active_window_name = new_window_name
         print(active_window_name)
-        # Second activate to get active window
+        # Second activity to get active window
+        if active_window_name == 'Google Chrome':
+            text = """tell app "Google Chrome" to get the url of the active tab of window 1"""
+            s = NSAppleScript.initWithSource_(NSAppleScript.alloc(), text)
+            result, err = s.executeAndReturnError_(None)
+            print(result.stringValue())
     time.sleep(2)
